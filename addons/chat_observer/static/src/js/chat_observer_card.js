efe94dfe5ce66aae6fe416491dd5f9f533b468d4
francis DAVID <contato@fdavid.com.br>
2026-06-01 08:21:02 -0300

    feat: add ChatObserverCard OWL component

--- addons/chat_observer/static/src/js/chat_observer_card.js
+++ addons/chat_observer/static/src/js/chat_observer_card.js
@@ -1,1 +1,20 @@
 /** @odoo-module **/
+import { Component } from "@odoo/owl";
+
+const STATUS_LABELS = {
+    red: "REQUER AÇÃO",
+    yellow: "ATENÇÃO",
+    green: "NORMAL",
+};
+
+export class ChatObserverCard extends Component {
+    static template = "chat_observer.Card";
+    static props = {
+        chat: Object,
+        onOpen: Function,
+    };
+
+    get statusLabel() {
+        return STATUS_LABELS[this.props.chat.color] || this.props.chat.color.toUpperCase();
+    }
+}
